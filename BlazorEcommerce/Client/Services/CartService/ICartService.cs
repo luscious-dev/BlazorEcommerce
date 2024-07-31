f76d33ad69bf12a80739d4fd6a337fac96747ada
Olawale Akin-Odanye <olawale.akinodanye@gmail.com>
2024-07-31 07:51:53 +0100

    Added shopping cart feature

---
+++ BlazorEcommerce/Client/Services/CartService/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Client.Services.CartService
+{
+    public interface ICartService
+    {
+        event Action OnChange;
+        Task AddToCart(CartItem cartItem);
+        Task<List<CartItem>> GetCartItems();
+        Task<List<CartProductResponse>> GetCartProducts();
+        Task RemoveCartItem(int productId, int productTypeId);
+        Task UpdateQuantity(CartProductResponse cartProductResponse);
+    }
+}
